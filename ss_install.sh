8fbaed01006c8eae29f7a9a4d95a8930c47c7770
arisskz6 <arisstz6@gmail.com>
2016-11-13 11:00:51 +0800

    Create ss_install.sh

---
+++ ss_install.sh
@@ -0,0 +1,29 @@
+apt-get -y update
+apt-get -y install  python-pip  python-m2crypto
+pip install shadowsocks
+
+cat > /etc/shadowsocks.json<<-EOF
+{
+    "server":"107.182.186.144",
+    "server_port":11982,
+    "local_address": "127.0.0.1",
+    "local_port":1080,
+    "password":"0322Qds",
+    "timeout":600,
+    "method":"aes-256-cfb",
+    "fast_open": false
+}
+EOF
+
+sslocal -c /etc/shadowsocks.json -d start
+apt-get -y install supervisor
+
+cat > /etc/supervisor/conf.d/shadowsocks.conf<<-EOF
+[program:shadowsocks]
+command=sslocal -c /etc/shadowsocks.json
+autorestart=true
+user=nobody
+EOF
+
+service supervisor start
+supervisorctl reload                                                           
