7bdd95afa362946f5b94fae19e3aabdbe3d9d9d0
arisskz6 <arisstz6@gmail.com>
2017-02-28 05:37:00 -0600

    Update polipo_install.sh

--- polipo_install.sh
+++ polipo_install.sh
@@ -5,7 +5,7 @@ rm -f /etc/polipo/config
 cp ~/shadowsocks/polipo.config /etc/polipo/config
 /etc/init.d/polipo restart
 alias hp='http_proxy=http://localhost:8123'
-echo "alias hp='http_proxy=http://localhost:8123'" >> ~/.bashrc
-source ~/.bashrc
+echo " alias hp='http_proxy=http://localhost:8123'" >> ~/.bashrc
+. ~/.bashrc
 http_proxy=http://localhost:8123 curl ip.gs
 cat ~/.bashrc | grep 'hp'
