a91b5f3d6f5cef9f58ef3887ad4ca9ef7e0144f7
arisskz6 <arisstz6@gmail.com>
2017-02-28 07:42:57 -0600

    Update ss_update.sh

--- ss_update.sh
+++ ss_update.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 rm -rf ~/shadowsocks
+
 apt -y install git
 cd ~
 git clone https://github.com/arisskz6/shadowsocks.git 
+chmod u+rx ~/shadowsocks/*.sh
+cp ~/shadowsocks/ss_update.sh ~/ss_install.and.update.sh
