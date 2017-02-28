288b5da1773f05aa8ff7768fd5154c0fb4166ca4
arisskz6 <arisstz6@gmail.com>
2017-02-28 07:17:31 -0600

    Update polipo_install.sh

--- polipo_install.sh
+++ polipo_install.sh
@@ -1,11 +1,15 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+LANG=zh_CN.UTF-8
 apt purge polipo -y > /dev/null 2>&1
 rm -f /etc/polipo/config > /dev/null 2>&1
 apt -y install polipo
 rm -f /etc/polipo/config
 cp ~/shadowsocks/polipo.config /etc/polipo/config
 /etc/init.d/polipo restart
-alias hp='http_proxy=http://localhost:8123'
+HP="alias hp='http_proxy=http://localhost:8123'"
 echo " alias hp='http_proxy=http://localhost:8123'" >> ~/.bashrc
-. ~/.bashrc
+source ~/.bashrc
 http_proxy=http://localhost:8123 curl ip.gs
 cat ~/.bashrc | grep 'hp'
