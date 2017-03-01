4b9a944d70d4d448a917e531604f3d02da7b06f3
arisskz6 <arisstz6@gmail.com>
2017-03-01 09:44:49 -0600

    Update ss_install.sh

--- ss_install.sh
+++ ss_install.sh
@@ -3,7 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH 
 
 echo "shadowsocks-python installation is starting..."
-ls /usr/local/bin/sslocal > /dev/null 2>&1 && pip uninstall shadowsocks
+ls /usr/local/bin/sslocal > /dev/null 2>&1 && pip uninstall shadowsocks -y
 rm -rf /etc/shadowsocks
 rm -f /etc/systemd/system/shadowsocks.service
 rm -rf /run/shadowsocks*
