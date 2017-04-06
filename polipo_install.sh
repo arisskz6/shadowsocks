dd1355bd7b1a2a5e6664834941bf7c0ffe461121
arisskz6 <arisstz6@gmail.com>
2017-04-06 19:37:00 +0800

    Update polipo_install.sh

--- polipo_install.sh
+++ polipo_install.sh
@@ -3,6 +3,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 apt-get --purge remove polipo -y > /dev/null 2>&1
 rm -f /etc/polipo/config > /dev/null 2>&1
+sed -i '/localhost:8123/d' ~/.bashrc
 apt-get -y install polipo curl
 rm -f /etc/polipo/config
 cp ~/shadowsocks/polipo.config /etc/polipo/config
