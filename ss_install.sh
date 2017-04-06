fd1aa5ea746b028a5b4882f9fbfd7a61f3f6cb68
arisskz6 <arisstz6@gmail.com>
2017-04-06 23:35:03 +0800

    Update ss_install.sh

--- ss_install.sh
+++ ss_install.sh
@@ -2,6 +2,14 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH 
 
+# Check if user is root
+if [ $(id -u) != "0" ]; then
+    echo "Error: You must be root to run this script, please use root to install ss"
+    exit
+fi
+
+clear
+echo "################################################"
 echo "shadowsocks-python installation is starting..."
 ls /usr/local/bin/sslocal && pip uninstall shadowsocks -y
 rm -rf /etc/shadowsocks
