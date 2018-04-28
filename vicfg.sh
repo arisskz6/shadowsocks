cbe09dfba498a621be021c63489a156dd96003d8
arisskz6 <arisstz6@gmail.com>
2018-04-28 14:32:24 +0800

    fix the vimrc path in vicfg.sh

--- vicfg.sh
+++ vicfg.sh
@@ -7,7 +7,7 @@ read -p "Before we configure vimrc,do you sure 'git' and 'vim' have been install
 if [ "$yn" = "y" ] || [ "$yn" = "Y"]
 then
 	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-	cp ~/shadowsocks/vimrc ~/.vimrc
+	cp ./vimrc ~/.vimrc
 	vim +PluginInstall +qall
 	echo 
 	echo "############"
